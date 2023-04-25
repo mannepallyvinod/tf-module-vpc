b9c817b7372726a8caa1844964e124455c06fbb2
mannepallyvinod <111588987+mannepallyvinod@users.noreply.github.com>
2023-04-25 11:57:37 +0530

    Delete vars.tf

--- vars.tf
+++
@@ -1,7 +0,0 @@
-variable "env" {}
-variable "vpc_cidr" {}
-variable "tags" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "default_vpc_id"{}
-variable "default_route_table"{}
